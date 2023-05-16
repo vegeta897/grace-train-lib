b9251db2df946798c2fec920a768fc265421e0c5
Devin Spikowski <vegeta897@gmail.com>
2023-05-16 08:31:24 -0400

    ✨ Add transition prop for conditional class

--- src/lib/Star.svelte
+++ src/lib/Star.svelte
@@ -3,6 +3,7 @@
 	export let rotate = 0
 	export let translate = { x: 0, y: 0 }
 	export let scale = 1
+	export let transition = false
 </script>
 
 <path
@@ -10,4 +11,15 @@
 		50 * scale},{translate.y - 50 * scale}) scale({scale})"
 	d="M50,0l11.226,34.549l36.327,0l-29.389,21.353l11.225,34.549l-29.389,-21.353l-29.389,21.353l11.225,-34.549l-29.389,-21.353l36.327,0l11.226,-34.549Z"
 	style:fill
+	class:transition
 />
+
+<style>
+	.transition {
+		/* Matches tailwind's transition-transform class */
+		/* Maybe expose this as a class prop instead */
+		transition-property: transform;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-duration: 150ms;
+	}
+</style>
