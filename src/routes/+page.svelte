c009d61e4b5ced372b81ef8df12ef8737cf59843
Devin Spikowski <vegeta897@gmail.com>
2023-06-22 08:50:39 -0400

    ✨ Add different preview sizes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,31 +3,37 @@
 	import Decal from '$lib/Decal.svelte'
 
 	const defaultTransform = { translate: { x: 375 / 2, y: 120 }, scale: 1.5, rotate: 0 }
+
+	const sizes = ['250px', '100px', '50px']
 </script>
 
 <svelte:head>
 	<title>Grace Train Component Library</title>
 </svelte:head>
-<div class="showcase">
-	<Body name="boxy">
-		<Decal name="star" transform={defaultTransform} />
-	</Body>
-	<Body name="tanky">
-		<Decal name="heart" transform={defaultTransform} />
-	</Body>
-	<Body name="boxy">
-		<Decal name="circle" transform={defaultTransform} />
-	</Body>
-</div>
+{#each sizes as size}
+	<div class="showcase" style="--column-size: {size}">
+		<Body name="boxy">
+			<Decal name="star" transform={defaultTransform} />
+		</Body>
+		<Body name="tanky">
+			<Decal name="heart" transform={defaultTransform} />
+		</Body>
+		<Body name="boxy">
+			<Decal name="circle" transform={defaultTransform} />
+		</Body>
+	</div>
+{/each}
 
 <style>
 	:global(html) {
 		background: #29185e;
+		padding: 1rem;
 	}
 	.showcase {
 		display: grid;
-		grid-template-columns: repeat(4, 250px);
+		grid-template-columns: repeat(4, var(--column-size));
 		row-gap: 2rem;
 		column-gap: 2rem;
+		margin-bottom: 1rem;
 	}
 </style>
