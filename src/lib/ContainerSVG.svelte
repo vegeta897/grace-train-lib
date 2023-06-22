7fa357e84ef2859d9d68b4f8cef333af3d0a6175
Devin Spikowski <vegeta897@gmail.com>
2023-06-22 09:19:31 -0400

    ✨ Export viewbox attribute

--- src/lib/ContainerSVG.svelte
+++ src/lib/ContainerSVG.svelte
@@ -1,14 +1,10 @@
 <script lang="ts">
 	let className = ''
 	export { className as class }
+	export let viewBox = '0 0 375 300'
 </script>
 
-<svg
-	xmlns="http://www.w3.org/2000/svg"
-	viewBox="0 0 375 300"
-	class={className}
-	width="100%"
->
+<svg xmlns="http://www.w3.org/2000/svg" {viewBox} class={className} width="100%">
 	<slot />
 </svg>
 
