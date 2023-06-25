8ddd68b3e8e84775b1741f6adc546196c72684b1
Devin Spikowski <vegeta897@gmail.com>
2023-06-25 05:32:30 -0400

    ✨ Add animated color change wheels

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,27 @@
 	import Body from '../lib/Body.svelte'
 	import Decal from '$lib/Decal.svelte'
 	import Wheels from '$lib/Wheels.svelte'
+	import WheelsChange from '$lib/WheelsChange.svelte'
+	import { onDestroy, onMount } from 'svelte'
+	import { COLORS } from '$lib/colors.js'
 
 	const defaultTransform = { translate: { x: 375 / 2, y: 120 }, scale: 1.5, rotate: 0 }
 
 	const sizes = ['250px', '100px', '50px']
+
+	const changingRimColors = [COLORS.POP, '#94f20d']
+	let changingRimColor = changingRimColors[0]
+
+	let changingRimInterval: number
+	onMount(() => {
+		changingRimInterval = setInterval(() => {
+			changingRimColor =
+				changingRimColors[
+					(changingRimColors.indexOf(changingRimColor) + 1) % changingRimColors.length
+				]
+		}, 2000)
+	})
+	onDestroy(() => clearInterval(changingRimInterval))
 </script>
 
 <svelte:head>
@@ -19,6 +36,10 @@
 		<Body name="tanky">
 			<Decal name="heart" transform={defaultTransform} slot="decals" />
 			<Wheels slot="wheels" rimColor="#94f20d" />
+		</Body>
+		<Body name="tanky">
+			<Decal name="heart" transform={defaultTransform} slot="decals" />
+			<WheelsChange slot="wheels" rimColor={changingRimColor} />
 		</Body>
 		<Body name="boxy">
 			<Decal name="circle" transform={defaultTransform} slot="decals" />
