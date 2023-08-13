099284505ac7c4f96de2925634458fd719ef1973
Devin Spikowski <vegeta897@gmail.com>
2023-08-13 00:09:28 -0400

    💄 Inline container svg styles

--- src/lib/ContainerSVG.svelte
+++ src/lib/ContainerSVG.svelte
@@ -4,15 +4,15 @@
 	export let viewBox = '0 0 375 300'
 </script>
 
-<svg xmlns="http://www.w3.org/2000/svg" {viewBox} class={className} width="100%">
+<svg
+	xmlns="http://www.w3.org/2000/svg"
+	{viewBox}
+	class={className}
+	width="100%"
+	stroke-linecap="round"
+	stroke-linejoin="round"
+	stroke-width="25"
+	style:overflow="visible"
+>
 	<slot />
 </svg>
-
-<style>
-	svg {
-		stroke-linecap: round;
-		stroke-linejoin: round;
-		stroke-width: 25px;
-		overflow: visible;
-	}
-</style>
