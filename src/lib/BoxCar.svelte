c5f6ac890396e3b19c14c85dcfc6863457f056d4
Devin Spikowski <vegeta897@gmail.com>
2023-08-23 03:38:08 -0400

    💫 Add color transition to body colors

--- src/lib/BoxCar.svelte
+++ src/lib/BoxCar.svelte
@@ -9,21 +9,34 @@
 	$: popColor = popColor || COLORS.POP
 </script>
 
-<rect x="162.5" y="12.5" width="50" height="25" style="fill:none;stroke:{popColor};" />
+<rect
+	x="162.5"
+	y="12.5"
+	width="50"
+	height="25"
+	class="color-transition"
+	style="fill:none;stroke:{popColor};"
+/>
 <rect
 	x="37.5"
 	y="212.5"
 	width="300"
 	height="25"
+	class="color-transition"
 	style="fill:{popColor};stroke:{popColor};"
 />
 <slot name="wheels" />
 <path
 	d="M37.5,212.5l-25,-175l350,0l-25,175l-300,0Z"
+	class="color-transition"
 	style="fill:{baseColor};stroke:{baseColor};"
 />
 <slot name="decals" />
-<path d="M12.5,212.5l350,0" style="fill:none;stroke:{baseColor};" />
+<path
+	d="M12.5,212.5l350,0"
+	class="color-transition"
+	style="fill:none;stroke:{baseColor};"
+/>
 {#if stripeColor !== 'none'}
 	<path
 		d="M366.143,99.934l-357.286,0l8.144,56.945l340.998,0l8.144,-56.945Z"
@@ -31,3 +44,11 @@
 	/>
 {/if}
 <slot name="toppers" />
+
+<style>
+	.color-transition {
+		transition-property: fill stroke;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-duration: 150ms;
+	}
+</style>
--- src/lib/TankerCar.svelte
+++ src/lib/TankerCar.svelte
@@ -9,21 +9,34 @@
 	$: popColor = popColor || COLORS.POP
 </script>
 
-<rect x="162.5" y="12.5" width="50" height="25" style="fill:none;stroke:{popColor};" />
+<rect
+	x="162.5"
+	y="12.5"
+	width="50"
+	height="25"
+	class="color-transition"
+	style="fill:none;stroke:{popColor};"
+/>
 <rect
 	x="37.5"
 	y="212.5"
 	width="300"
 	height="25"
+	class="color-transition"
 	style="fill:{popColor};stroke:{popColor};"
 />
 <slot name="wheels" />
 <path
 	d="M37.5,200c0,0 -25,0 -25,-75c0,-75 25,-75 25,-75l300,0c0,0 25,0 25,75c-0,75 -25,75 -25,75l-300,-0Z"
+	class="color-transition"
 	style="fill:{baseColor};stroke:{baseColor};"
 />
 <slot name="decals" />
-<path d="M12.5,212.5l350,0" style="fill:none;stroke:{baseColor};" />
+<path
+	d="M12.5,212.5l350,0"
+	class="color-transition"
+	style="fill:none;stroke:{baseColor};"
+/>
 {#if stripeColor !== 'none'}
 	<path
 		d="M374.044,99.934l-373.024,0c-0,0 -1.02,9.223 -1.02,28.473c0,15.457 1.763,28.472 1.763,28.472l371.616,0c0,0 1.663,-13.268 1.663,-28.472c0,-17.099 -0.998,-28.473 -0.998,-28.473Z"
@@ -31,3 +44,11 @@
 	/>
 {/if}
 <slot name="toppers" />
+
+<style>
+	.color-transition {
+		transition-property: fill stroke;
+		transition-timing-function: cubic-bezier(0.4, 0, 0.2, 1);
+		transition-duration: 150ms;
+	}
+</style>
