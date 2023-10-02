4091132bb7e60f2a29797150fbac7404e8a00a22
Devin Spikowski <vegeta897@gmail.com>
2023-10-02 05:39:00 -0400

    ♻️ Export function to get default decal params

--- src/lib/components/decal/index.ts
+++ src/lib/components/decal/index.ts
@@ -2,7 +2,7 @@ import type { ComponentType, SvelteComponent } from 'svelte'
 import * as Heart from './Heart.svelte'
 import * as Star from './Star.svelte'
 import * as Circle from './Circle.svelte'
-import type { ParamDefinition } from './params'
+import { getDefaultParamsObject, type ParamDefinition } from './params'
 
 export const DECAL_NAMES = ['star', 'heart', 'circle'] as const
 export type DecalName = (typeof DECAL_NAMES)[number]
@@ -11,7 +11,11 @@ function defineDecal(importObject: {
 	default: ComponentType<SvelteComponent>
 	paramConfig: ParamDefinition[]
 }) {
-	return { component: importObject.default, paramConfig: importObject.paramConfig }
+	return {
+		component: importObject.default,
+		paramConfig: importObject.paramConfig,
+		getDefaultParamsObject: () => getDefaultParamsObject(importObject.paramConfig),
+	}
 }
 
 export const decalDefs = {
