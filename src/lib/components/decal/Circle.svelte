eeba563ceec2d4bfedd59fecd34d763b9ba2e280
Devin Spikowski <vegeta897@gmail.com>
2023-11-25 03:11:00 -0500

    👔 Improve circle decal bounding box

--- src/lib/components/decal/Circle.svelte
+++ src/lib/components/decal/Circle.svelte
@@ -1,15 +1,24 @@
 <script lang="ts" context="module">
 	import { defineScalar } from './params'
 
+	const getRx = (pinch: number) => 50 - Math.max(0, pinch - 0.5) * 60
+	const getRy = (pinch: number) => 50 - Math.max(0, 0.5 - pinch) * 60
+
 	export const paramConfig = [defineScalar('pinch', 0.5), defineScalar('hollow')]
+
+	type Params = { pinch: number; hollow: number }
+	export const getBoundingBox = (params: Params) => ({
+		width: getRx(params.pinch) * 2,
+		height: getRy(params.pinch) * 2,
+	})
 </script>
 
 <script lang="ts">
 	$$restProps
 	export let fill: string
-	export let params: { pinch: number; hollow: number }
-	$: rx = 50 - Math.max(0, params.pinch - 0.5) * 60
-	$: ry = 50 - Math.max(0, 0.5 - params.pinch) * 60
+	export let params: Params
+	$: rx = getRx(params.pinch)
+	$: ry = getRy(params.pinch)
 	$: hrx = params.hollow * (rx - 10)
 	$: hry = params.hollow * (ry - 10)
 	$: hollowPath =
