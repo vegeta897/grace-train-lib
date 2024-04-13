0be60f599e33bc8c00fe07803be3dbd89a698783
Devin Spikowski <vegeta897@gmail.com>
2024-04-13 07:12:52 -0400

    🧑‍💻 Improve topper line debug visuals

--- src/lib/components/Body.svelte
+++ src/lib/components/Body.svelte
@@ -1,8 +1,9 @@
 <script lang="ts" context="module">
 	import { COLOR_NAMES } from '$lib'
 	import type { BodyName } from '$lib/data'
+	// import { getYposition } from '.'
 	import Wheels from './Wheels.svelte'
-	import { bodyDefs } from './body/index.js'
+	import { bodyDefs } from './body'
 </script>
 
 <script lang="ts">
@@ -17,9 +18,10 @@
 	$: baseColor = props.baseColor || COLOR_NAMES.BASE.BASE
 	$: popColor = props.popColor || COLOR_NAMES.POP.POP
 	$: stripeColor = props.stripeColor || 'none'
+	$: bodyDef = bodyDefs[name]
 </script>
 
-<svelte:component this={bodyDefs[name].component} {baseColor} {popColor} {stripeColor}>
+<svelte:component this={bodyDef.component} {baseColor} {popColor} {stripeColor}>
 	<slot name="wheels" slot="wheels">
 		<Wheels />
 	</slot>
@@ -28,14 +30,18 @@
 			<slot name="decals" />
 		</g>
 	</svelte:fragment>
-	<slot name="toppers" slot="toppers" topLine={bodyDefs[name].topperLine} />
+	<slot name="toppers" slot="toppers" topLine={bodyDef.topperLine} />
 	<!-- <svelte:fragment slot="toppers">
-		<slot name="toppers" topLine={bodyDefs[name].topperLine} />
-		{#each bodyDefs[name].topperLine.points as [tpx, tpy]}
-			<circle cx={tpx} cy={tpy} r="3" fill="none" stroke-width="2" stroke="red" />
+		<slot name="toppers" topLine={bodyDef.topperLine} />
+		{#each bodyDef.topperLine.points as [tpx, tpy]}
+			<circle cx={tpx} cy={tpy} r="3" fill="none" stroke-width="2" stroke="#fff" />
+		{/each}
+		{#each Array(200) as _, i}
+			{@const x = bodyDef.topperLine.startX + bodyDef.topperLine.width * (i / 200)}
+			<circle cx={x} cy={getYposition(x, bodyDef.topperLine.points)} r="2" fill="#fff8" />
 		{/each}
 	</svelte:fragment> -->
 </svelte:component>
 <defs>
-	<clipPath id="{name}-decal-clip"> <path d={bodyDefs[name].decalClipPath} /> </clipPath>
+	<clipPath id="{name}-decal-clip"> <path d={bodyDef.decalClipPath} /> </clipPath>
 </defs>
